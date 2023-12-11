dc7cd0a41bec8f7e69f4b72501aecd2f769cc4f7
wellington-100 <vasile.andruscenco@gmail.com>
2023-12-11 20:23:07 +0200

    serializers/code-encode/

--- women/views.py
+++ women/views.py
@@ -4,11 +4,23 @@ from rest_framework.response import Response
 from .models import Women
 from .serializers import WomenSerializer
 from rest_framework.views import APIView
+from django.forms import model_to_dict
 
 
 class WomenAPIView(APIView):
     def get(self, request):
-        return Response({'title': 'Angelina Jolie'})
+        w = Women.objects.all()
+        return Response({'posts': WomenSerializer(w, many=True).data})
+
+    def post(self, request):
+        serializer = WomenSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        post_new = Women.objects.create(
+            title = request.data['title'],
+            content = request.data['content'],
+            cat_id = request.data['cat_id']
+        )
+        return Response({'post': WomenSerializer(post_new).data})
 
 # class  WomenAPIView(generics.ListAPIView):
 #     queryset = Women.objects.all()
