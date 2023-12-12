e19484c86216b720e175bbc6e5f0bec64bc2c0a6
wellington-100 <vasile.andruscenco@gmail.com>
2023-12-12 16:04:04 +0200

    APIList,APIUpdate,APIView,routers,etc.

--- drfsite/urls.py
+++ drfsite/urls.py
@@ -16,9 +16,46 @@ Including another URLconf
 """
 from django.contrib import admin
 from women.views import *
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+
+class MyCustomRouter(routers.SimpleRouter):
+    routes = [
+        routers.Route(
+            url=r'^{prefix}$',
+            mapping={'get': 'list'},
+            name='{basename}-list',
+            detail=False,
+            initkwargs={'suffix': 'List'}
+        ),
+        routers.Route(
+            url=r'^{prefix}/{lookup}$',
+            mapping={'get': 'retrieve'},
+            name='{basename}-detail',
+            detail=True,
+            initkwargs={'suffix': 'Detail'}
+        )
+    ]
+
+router = MyCustomRouter()
+# router = routers.SimpleRouter()
+router.register(r'women', WomenViewSet, basename='women')
+print(router.urls)
+
+
+
+# router = routers.DefaultRouter()
+# # router = routers.SimpleRouter()
+# router.register(r'women', WomenViewSet, basename='women')
+# print(router.urls)
+
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/womenlist/', WomenAPIView.as_view()),
+    path('api/v1/', include(router.urls)), # http://127.0.0.1:8000/api/v1/women/
+    # path('api/v1/womenlist/', WomenViewSet.as_view({'get': 'list'})),
+    # path('api/v1/womenlist/<int:pk>/', WomenViewSet.as_view({'put': 'update'})),
+    # path('api/v1/womendetail/<int:pk>/', WomenAPIDetailView.as_view()),
 ]
--- women/views.py
+++ women/views.py
@@ -1,27 +1,90 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from rest_framework.response import Response
-from .models import Women
+from .models import Women, Category
 from .serializers import WomenSerializer
 from rest_framework.views import APIView
 from django.forms import model_to_dict
+from rest_framework.decorators import action
 
 
-class WomenAPIView(APIView):
-    def get(self, request):
-        w = Women.objects.all()
-        return Response({'posts': WomenSerializer(w, many=True).data})
+class WomenViewSet(viewsets.ModelViewSet):
+    # queryset = Women.objects.all()
+    serializer_class = WomenSerializer  
 
-    def post(self, request):
-        serializer = WomenSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        post_new = Women.objects.create(
-            title = request.data['title'],
-            content = request.data['content'],
-            cat_id = request.data['cat_id']
-        )
-        return Response({'post': WomenSerializer(post_new).data})
 
+    def get_queryset(self):
+        pk = self.kwargs.get('pk')
+        if not pk:
+            return Women.objects.all()
+        
+        return Women.objects.filter(pk=pk)
+       
+
+    @action(methods=['get'], detail=True)
+    def category(self, request, pk=None):
+        cats = Category.objects.get(pk=pk)
+        return Response({'cats': cats.name})
+
+
+######################################################
+# class WomenAPIList(generics.ListCreateAPIView):
+#     queryset = Women.objects.all()
+#     serializer_class = WomenSerializer
+
+# class WomenAPIUpdate(generics.UpdateAPIView):
+#     queryset = Women.objects.all()
+#     serializer_class = WomenSerializer
+
+# class WomenAPIDetailView(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Women.objects.all()
+#     serializer_class = WomenSerializer
+
+######################################################
+
+
+
+######################################################
+# class WomenAPIView(APIView):
+#     def get(self, request):
+#         w = Women.objects.all()
+#         return Response({'posts': WomenSerializer(w, many=True).data})
+
+#     def post(self, request):
+#         serializer = WomenSerializer(data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response({'post': serializer.data})
+    
+#     def put(self, request, *args, **kwargs):
+#         pk = kwargs.get("pk", None)
+#         if not pk:
+#             return Response({"error": "Method PUT not allowed"})
+#         try:
+#             instance = Women.objects.get(pk=pk)
+#         except:
+#             return Response({"error": "Object does not exists"})
+
+#         serializer = WomenSerializer(data=request.data, instance=instance)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response({"post": serializer.data})
+
+#     def delete(self, request, *args, **kwargs):
+#         pk = kwargs.get("pk", None)
+#         if not pk:
+#             return Response({"error": "Method DELETE not allowed"}, status=400)
+        
+#         try:
+#             instance = Women.objects.get(pk=pk)
+#         except Women.DoesNotExist:
+#             return Response({"error": "Object does not exist"}, status=404)
+
+#         instance.delete()
+#         return Response({"message": "Deleted"}, status=204)
+######################################################
+
+######################################################
 # class  WomenAPIView(generics.ListAPIView):
 #     queryset = Women.objects.all()
 #     serializer_class = WomenSerializer
